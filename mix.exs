99d4cbb527cdff82ad1f9d4d0eba14ed188bbc29
Ryan Irish <>
2024-03-24 22:06:16 -0400

    remove tailwind ref

--- mix.exs
+++ mix.exs
@@ -41,14 +41,7 @@ defmodule Ryancirish.MixProject do
       {:phoenix_live_view, "~> 0.20.2"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
-      {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},      
-      {:heroicons,
-       github: "tailwindlabs/heroicons",
-       tag: "v2.1.1",
-       sparse: "optimized",
-       app: false,
-       compile: false,
-       depth: 1},
+      {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.5"},
       {:finch, "~> 0.13"},
       {:telemetry_metrics, "~> 0.6"},
@@ -72,10 +65,9 @@ defmodule Ryancirish.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
-      "assets.build": ["tailwind ryancirish", "esbuild ryancirish"],
+      "assets.setup": ["esbuild.install --if-missing"],
+      "assets.build": ["esbuild ryancirish"],
       "assets.deploy": [
-        "tailwind ryancirish --minify",
         "esbuild ryancirish --minify",
         "phx.digest"
       ]
